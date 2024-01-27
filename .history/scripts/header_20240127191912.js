3ca8992620b152736db7b9d0279f6f0dac9274b9
sem1103 <sem.sem.geo.az1998@mail.ru>
2024-01-27 20:23:07 +0400

    first commit

---
+++ .history/scripts/header_20240127191912.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    let stickyElement = $('#page > nav');
+
+    let stickyHeight = stickyElement.height();
+
+
+    $(window).scroll(function () {
+        let scrollPosition = $(window).scrollTop();
+        if (scrollPosition > stickyHeight) {
+            stickyElement.addClass('sticky__elem')
+        } else {
+            stickyElement.removeClass('sticky__elem')
+        }
+
+
+    });
+
+    $('#menu__btn').click(openCloseMenuHandler);
+    $('#close__btn').click(openCloseMenuHandler)
+
+    function openCloseMenuHandler() {
+        $('body').toggleClass('active__menu')
+    }
+
+   
+        $(".nav__header").click(function(){
+            let submenu = $(this).next(".footer__submenu");
+            submenu.slideToggle();
+            $(".footer__submenu").not(submenu).slideUp();
+            });
+
+
+
+    
+
+});
